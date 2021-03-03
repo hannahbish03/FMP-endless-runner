d8e98241e1807eade9db7bbae91eb09fd120445c
hannahbish <510894@chrome.escg.ac.uk>
2021-03-03 10:44:29 +0000

    uploading my game to git

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothSpeed );
+        transform.position = smoothedPosition; 
+    }
+
+
+
+
+}
+
+
+
