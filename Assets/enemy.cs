1ffb267210225efc664fd55acb65d6af45f15286
hannahbish <510894@chrome.escg.ac.uk>
2021-03-15 10:25:32 +0000

    no message

---
+++ Assets/enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 2f;
+    public Rigidbody2D rb;
+    public LayerMask groundLayers;
+
+    public Transform groundCheck;
+
+    bool isFacingRight = true;
+
+    RaycastHit2D hit;
+
+    private void Update()
+    {
+        hit = Physics2D.Raycast(groundCheck.position, -transform.up, 1f, groundLayers);
+    }
+
+    private void FixedUpdate()
+    {
+        if (hit.collider != false)
+        {
+            Debug.Log("hitting ground");
+         } 
+          else
+          {
+            Debug.Log("not hitting ground");
+          }
+    }
+}
